36e53fc51cf4be032bac95c318a0df7a1a22919f
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-02 04:58:41 +0900

    rearange directories

--- parse_query.go
+++ parse_query.go
@@ -3,16 +3,33 @@ package main
 import (
 	"github.com/PuerkitoBio/goquery"
 	"fmt"
+	"strconv"
+	"log"
 )
 
 type Query struct {
 	N int
-	clusterId string
+	ClusterId string
 	query string
 }
 
+func (q *Query) parseQuery(args map[string]interface{}) {
+	var casted = make(map[string]string)
+	for key, val := range args {
+		casted[key], _ = val.(string)
+	}
+
+	q.ClusterId = casted["-C"]
+	n, err := strconv.Atoi(casted["-N"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	q.N = n
+}
+
 func (q *Query) setClusterIdQuery() {
-	q.query = SCHOLAR_URL + "scholar?cluster=" + q.clusterId + "&num=" + fmt.Sprintf("%d", q.N)
+	q.query = SCHOLAR_URL + "scholar?hl=en&cluster=" + q.ClusterId + "&num=" + fmt.Sprintf("%d", q.N)
+	fmt.Println(q.query)
 }
 
 func (q *Query) NewQuery() (*goquery.Document, error) {
