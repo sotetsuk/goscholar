6ae0eb423b93ed9f7705996eb1210839b009c975
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-02 23:57:45 +0900

    update utils.go

--- utils.go
+++ utils.go
@@ -6,7 +6,6 @@ import (
 )
 
 const (
-	SCHOLAR_URL = "https://scholar.google.co.jp/"
 	WHOLE_ARTICLE_SELECTOR   = ".gs_r"
 	ARTICLE_TITLE_SELECTOR   = ".gs_rt > a"
 	ARTICLE_HEADER_SELECTOR  = ".gs_a"
