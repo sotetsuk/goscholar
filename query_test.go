f8ce087a99b46a6d3b23723ba3d1988caa801787
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-03 00:12:19 +0900

    split long codes to separated functions

--- query_test.go
+++ query_test.go
@@ -5,36 +5,39 @@ import (
 	"fmt"
 )
 
-func TestQuery(t *testing.T) {
-	// test ScholarQuery
-	// test NewQuery()
-	// test search query
-	// test find query
-	find_params := map[string]string{"cluster_id":"8108748482885444188", "num":""}
-	find_q := ScholarQuery{"find", find_params}
-	find_query, err := find_q.findQuery()
+func TestFindQuery(t *testing.T) {
+	// set params
+	params := map[string]string{"cluster_id":"8108748482885444188", "num":""}
+	q := ScholarQuery{"find", params}
+
+	// exec findQuery()
+	query, err := q.findQuery()
 	if err != nil {
 		t.Error(fmt.Sprintf("findQuery() failed to return values: %v", err.Error()))
 	}
-	find_expected := "https://scholar.google.co.jp/scholar?hl=en&cluster=8108748482885444188&num="
-	if find_query != find_expected {
-		t.Error(fmt.Sprintf("findQuery() returned unexpected values\n  Expected: %v\n  Query   : %v", find_expected, find_query))
-	}
-	fmt.Printf("findQuery() returns %v\n", find_query)
 
-	// test cite query
-	cite_params := map[string]string{"cluster_id":"8108748482885444188", "after":"2012", "start":"20", "num":"40"}
-	cite_q := ScholarQuery{"cite", cite_params}
-	cite_query, err := cite_q.citeQuery()
+	// check the results and expected results
+	expected := "https://scholar.google.co.jp/scholar?hl=en&cluster=8108748482885444188&num="
+	if query != expected {
+		t.Error(fmt.Sprintf("findQuery() returned unexpected values\n  Expected: %v\n  Query   : %v", expected, query))
+	}
+	fmt.Printf("findQuery() returns %v\n", query)
+}
+
+func TestCiteQuery(t *testing.T) {
+	// set params
+	params := map[string]string{"cluster_id":"8108748482885444188", "after":"2012", "start":"20", "num":"40"}
+	q := ScholarQuery{"cite", params}
+
+	// exec citeQuery()
+	cite_query, err := q.citeQuery()
 	if err != nil {
 		t.Error(fmt.Sprintf("citeQuery() failed to return values: %v", err.Error()))
 	}
-	cite_expected := "https://scholar.google.co.jp/scholar?hl=en&cites=8108748482885444188&as_ylo=2012&as_yhi=&start=20&num=40"
-	if cite_query != cite_expected {
-		t.Error(fmt.Sprintf("citeQuery() returned unexpected values\n  Expected: %v\n  Query   : %v", cite_expected, cite_query))
+	expected := "https://scholar.google.co.jp/scholar?hl=en&cites=8108748482885444188&as_ylo=2012&as_yhi=&start=20&num=40"
+	// check the results and expected results
+	if cite_query != expected {
+		t.Error(fmt.Sprintf("citeQuery() returned unexpected values\n  Expected: %v\n  Query   : %v", expected, cite_query))
 	}
 	fmt.Printf("citeQuery() returns %v\n", cite_query)
-
-	// test CitePopUpQuery
-	// test NewQuery()
 }
