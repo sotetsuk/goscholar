98a8c7fa12a826f0e3026fa84f8ace202b8b0feb
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-02 23:57:13 +0900

    add query generating function and its test

--- query.go
+++ query.go
@@ -1,42 +1,83 @@
 package go_scholar
 
 import (
-	"github.com/PuerkitoBio/goquery"
 	"fmt"
-	"strconv"
-	"log"
+	"errors"
 )
 
-type Query struct {
-	N int
-	NCiting int
-	ClusterId string
-	query string
+const (
+	SCHOLAR_URL = "https://scholar.google.co.jp/"
+)
+
+var (
+	SEARCH_URL     = SCHOLAR_URL + "scholar?hl=en&q=%v&as_ylo=%v&as_yhi=%v&start=%v&num=%v"
+	FIND_URL       = SCHOLAR_URL + "scholar?hl=en&cluster=%v&num=%v"
+	CITE_URL       = SCHOLAR_URL + "scholar?hl=en&cites=%v&as_ylo=%v&as_yhi=%v&start=%v&num=%v"
+	CITE_POPUP_URL = SCHOLAR_URL + "scholar?q=info:%s:scholar.google.com/&output=cite&scirp=0&hl=en"
+)
+
+type ScholarQuery struct {
+	method     string
+	parameters map[string]string
 }
 
-func (q *Query) parseQuery(args map[string]interface{}) {
-	var casted = make(map[string]string)
-	for key, val := range args {
-		casted[key], _ = val.(string)
-	}
-
-	q.ClusterId = casted["-C"]
-	n, err := strconv.Atoi(casted["-N"])
-	if err != nil {
-		log.Fatal(err)
-	}
-	q.N = n
-	nCiting, err := strconv.Atoi(casted["-M"])
-	if err != nil {
-		log.Fatal(err)
-	}
-	q.NCiting = nCiting
+type CitePopUpQuery struct {
+	info string
 }
 
-func (q *Query) setClusterIdQuery() {
-	q.query = SCHOLAR_URL + "scholar?hl=en&cluster=" + q.ClusterId + "&num=" + fmt.Sprintf("%d", q.N)
+func (q *ScholarQuery) NewQuery() (string, error) {
+	if q.method == "search" {
+
+	} else if q.method == "find" {
+		return q.findQuery()
+	} else if q.method == "cite" {
+		return q.citeQuery()
+	}
+
+	return "", errors.New("Wrong method was called. Only search, find and cite are usable.")
 }
 
-func (q *Query) NewQuery() (*goquery.Document, error) {
-	return goquery.NewDocument(q.query)
+func (q *ScholarQuery) searchQuery() (string, error) {
+	return "", nil
+}
+
+func (q *ScholarQuery) findQuery() (string, error) {
+	// parse parameters
+	cluster_id, exists := q.parameters["cluster_id"]
+	if !exists {
+		return "", errors.New("Parameter cluster_id is needed!")
+	}
+
+	// fill nil of option by blank string
+	num, exists := q.parameters["num"]
+	if !exists {
+		num = ""
+	}
+
+	return fmt.Sprintf(FIND_URL, cluster_id, num), nil
+}
+
+func (q *ScholarQuery) citeQuery() (string, error) {
+	// parse parameters
+	cluster_id, ok := q.parameters["cluster_id"]
+	if !ok {
+		return "", errors.New("Parameter cluster_id is needed!")
+	}
+
+	// fill nil options by blank string
+	params := []string{"before", "after", "num", "start"}
+	for _, op := range params {
+		if _, exists := q.parameters[op]; !exists {
+			q.parameters[op] = ""
+		}
+	}
+
+	return fmt.Sprintf(CITE_URL, cluster_id, q.parameters["after"], q.parameters["before"], q.parameters["start"], q.parameters["num"]), nil
+}
+
+func (q *CitePopUpQuery) NewQuery() (string, error) {
+	if q.info == "" {
+		return "", errors.New("info attribute is nil!")
+	}
+	return fmt.Sprintf(CITE_POPUP_URL, q.info), nil
 }
