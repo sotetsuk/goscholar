33768451abe7b33b4e3bdd0b764ca60733f2dcab
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-05-07 15:17:39 +0900

    fix query_test url

--- query_test.go
+++ query_test.go
@@ -14,8 +14,8 @@ func TestSearchUrl(t *testing.T) {
 }
 
 func TestFindUrl(t *testing.T) {
-	expected := "https://scholar.google.co.jp/scholar?hl=en&cluster=8108748482885444188&num=1"
-	q := &Query{cluster_id:"8108748482885444188"}
+	expected := "https://scholar.google.co.jp/scholar?hl=en&cluster=5362332738201102290&num=1"
+	q := &Query{cluster_id:"5362332738201102290"}
 
 	if actual := q.FindUrl(); actual != expected {
 		t.Error(TestErr{expected:expected, actual:actual})
@@ -23,8 +23,8 @@ func TestFindUrl(t *testing.T) {
 }
 
 func TestCiteUrl(t *testing.T) {
-	expected := "https://scholar.google.co.jp/scholar?hl=en&cites=8108748482885444188&as_ylo=2012&as_yhi=&num=40&start=20"
-	q := &Query{cluster_id:"8108748482885444188", after:"2012", num:"40", start:"20"}
+	expected := "https://scholar.google.co.jp/scholar?hl=en&cites=5362332738201102290&as_ylo=2012&as_yhi=&num=40&start=20"
+	q := &Query{cluster_id:"5362332738201102290", after:"2012", num:"40", start:"20"}
 
 	if actual := q.CiteUrl(); actual != expected {
 		t.Error(TestErr{expected:expected, actual:actual})
